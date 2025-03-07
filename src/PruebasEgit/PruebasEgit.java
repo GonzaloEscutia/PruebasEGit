1e3709e52b597b372f63163aa5de466a4cbc4eee
Gonzalo <gonzaloescufer@gmail.com>
2025-03-07 12:24:06 +0100

    Segundo Commit

--- src/PruebasEgit/PruebasEgit.java
+++ src/PruebasEgit/PruebasEgit.java
@@ -3,8 +3,8 @@ package PruebasEgit;
 public class PruebasEgit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Pruebas EGit");
+		System.out.println("Primer Cambio");
 	}
 
 }
