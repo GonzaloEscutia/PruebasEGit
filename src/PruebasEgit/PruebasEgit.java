cb1748f2b0687ffb38924dcd6a209edc1864a7ad
Gonzalo <gonzaloescufer@gmail.com>
2025-03-07 13:05:08 +0100

    Primer commit desde rama1

--- src/PruebasEgit/PruebasEgit.java
+++ src/PruebasEgit/PruebasEgit.java
@@ -5,6 +5,6 @@ public class PruebasEgit {
 	public static void main(String[] args) {
 		System.out.println("Pruebas EGit");
 		System.out.println("Primer Cambio");
+		System.out.println("Cambio desde rama1");
 	}
-
 }
